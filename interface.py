d9e6f1da64d66f330faac221aec03c3c008610c0
Josh Albrecht <thejash@gmail.com>
2016-05-20 13:49:52 -0700

    unbreaking things. last commit was bad

--- interface.py
+++ interface.py
@@ -17,10 +17,10 @@ one_week_in_seconds = 14*one_day_in_seconds
 
 # class ActivityLog:
   # FILE_NAME = "activity.log"
-
+  
   # def __init__(self):
     # self.data = []
-
+    
   # @staticmethod
   # def load():
     # log = ActivityLog()
@@ -28,16 +28,16 @@ one_week_in_seconds = 14*one_day_in_seconds
     # log.data = json.loads(inFile.read(), parse_float=decimal.Decimal, parse_int=decimal.Decimal)
     # inFile.close()
     # return log
-
+    
   # def save(self):
     # outFile = open(ActivityLog.FILE_NAME, "wb")
     # outFile.write(json.dumps(self.data, default=_serializer, sort_keys=True, indent=2))
     # outFile.close()
-
+    
   # def pop(self):
     # self.data.pop()
     # self.save()
-
+    
   # def add(self, newData):
     # self.data.append(newData)
     # self.save()
@@ -45,7 +45,7 @@ one_week_in_seconds = 14*one_day_in_seconds
 def prompt(message):
   print(message)
   return raw_input()
-
+  
 def read_map(message):
   data = {}
   secondary_separator = ";"
@@ -73,7 +73,7 @@ def read_map(message):
       assert len(split_data) == 2
       data[split_data[0].strip()] = read_min_max(split_data[1].strip())
   return data
-
+      
 def _convert_values_to_decimal(data):
   for key in data:
     #TODO:  remove this once everything is converted
@@ -82,13 +82,13 @@ def _convert_values_to_decimal(data):
     else:
       newVal = [decimal.Decimal(data[key]), decimal.Decimal(data[key])]
     data[key] = newVal
-
+    
 def _serializer(object):
   if isinstance(object, decimal.Decimal):
     return str(object)
   else:
     return json.dumps(object)
-
+    
 def parse_time_from_user(user_data, last_recorded_time):
   #figure out how long it took, in seconds
   duration = None
@@ -126,7 +126,7 @@ def load_all_goals():
     goal = Goal.load_from_file(file_name)
     goals[goal.id] = goal
   return goals
-
+  
 def parse_goal_from_user(user_data, goals):
   user_data = user_data.strip()
   #first try to find by goal id
@@ -157,15 +157,15 @@ class Goal:
     nextId = int(inFile.read().strip())
     inFile.close()
     return nextId
-
-  @staticmethod
+  
+  @staticmethod  
   def increment_next_id():
     nextId = Goal.get_next_id()
     nextId += 1
     outFile = open(Goal.ID_FILE, "wb")
     outFile.write(str(nextId))
     outFile.close()
-
+  
   def __init__(self):
     self.id = None
     self.description = ""
@@ -179,11 +179,11 @@ class Goal:
     self.last_saved_at = None
     self.progress = []
     self.requires = []
-
+    
   @staticmethod
   def file_name_from_id(file_id):
     return "goals/"+str(file_id)+".json"
-
+    
   @staticmethod
   def load_from_file(file_name):
     #file_name = Goal.file_name_from_id(file_id)
@@ -199,7 +199,7 @@ class Goal:
     goal.thoughts = thoughts.strip()
     goal._finish_load()
     return goal
-
+    
   def load_from_user(self, tags):
     self.created_at = NOW
     #get the next highest id automatically
@@ -233,7 +233,7 @@ class Goal:
     #save the goal
     self.save()
     Goal.increment_next_id()
-
+    
   def save(self):
     """Serialize mostly to JSON.  Have to handle Decimals specially, and the thoughts field, which I want to be directly editable text"""
     self.last_saved_at = NOW
@@ -244,7 +244,7 @@ class Goal:
     outFile = open(Goal.file_name_from_id(self.id), 'wb')
     outFile.write(data)
     outFile.close()
-
+    
   def _finish_load(self):
     _convert_values_to_decimal(self.value_components)
     _convert_values_to_decimal(self.cost_components)
@@ -263,7 +263,7 @@ class Goal:
       self.last_saved_at = decimal.Decimal(self.last_saved_at)
     if self.completed_at:
       self.completed_at = decimal.Decimal(self.completed_at)
-
+    
   def add_time(self, start_time, end_time, focus, notes, is_complete):
     #put a new entry into progress
     self.progress.append([start_time, end_time, focus, notes])
@@ -273,16 +273,16 @@ class Goal:
     # log = ActivityLog.load()
     # log.add([self.id, start_time, end_time])
     self.save()
-
+    
   def undo_add_time(self):
     #this happens when the user inputs an incorrect value
-    #pop the last entry off our time stack.
+    #pop the last entry off our time stack.  
     self.progress.pop()
     ##also update the activity log
     # log = ActivityLog.load()
     # log.pop()
     self.save()
-
+    
   def get_effort_in_interval(self, start_time, end_time):
     total_effort = decimal.Decimal(0)
     for start, end, focus, notes in self.progress:
@@ -293,52 +293,52 @@ class Goal:
         continue
       total_effort += effort * focus
     return total_effort
-
+    
   @property
   def last_updated_at(self):
     if len(self.progress) <= 0:
       return None
     else:
       return self.progress[-1][1]
-
+    
   @property
   def total_estimated_cost(self):
     return sum([(r[0]+r[1])/2 for r in self.cost_components.values()])
-
+    
   @property
   def total_estimated_value(self):
     return sum([(r[0]+r[1])/2 for r in self.value_components.values()])
-
+    
   @property
   def total_estimated_time(self):
     return sum([(r[0]+r[1])/2 for r in self.time_components.values()])
-
+    
   @property
   def net_estimated_value(self):
     return self.total_estimated_value - self.total_estimated_cost
-
+    
   @property
   def value_rate(self):
     return self.net_estimated_value / self.total_estimated_time
-
+    
   @property
   def title(self):
     return self.description.split('.')[0]
-
+  
   @property
   def is_complete(self):
     return self.completed_at != None
-
+    
 def create_goal(user_data, tags):
   goal = Goal()
   goal.load_from_user(tags)
   goal.save()
-
+  
 def add_time(user_data, goal_dict, prev_entry):
   user_data = user_data.strip()
   is_complete = False
   if prev_entry == None:
-    last_recorded_time = decimal.Decimal(str(time.time()))
+    last_recorded_time = decimal.Decimal("1339427450.91")
   else:
     last_recorded_time = prev_entry.end_time
   goal_amount_pairs = []
@@ -385,7 +385,7 @@ def add_time(user_data, goal_dict, prev_entry):
   #update the goals
   for goal, amount in goal_amount_pairs:
     goal.add_time(last_recorded_time, end_time, decimal.Decimal(amount), notes, is_complete)
-
+  
 def edit(user_data, goal_dict):
   #figure out what goal the user is referring to
   goal = parse_goal_from_user(goal_data, goal_dict)
@@ -503,16 +503,16 @@ def summarize(goals, user_data, for_tags = False):
     print "-" * (pad_spaces+21)
     for tag in get_labels_sorted_by_duration(tagged_entries):
       total_tag_duration = get_duration_for_entries(tagged_entries[tag])
-      print "%s - %s - %0d %%" % (pad(tag, pad_spaces),
-        get_human_readable_duration(total_tag_duration),
+      print "%s - %s - %0d %%" % (pad(tag, pad_spaces), 
+        get_human_readable_duration(total_tag_duration), 
         round(100*total_tag_duration/total_duration))
     print "-" * (pad_spaces+21)
     for tag in get_labels_sorted_by_duration(tagged_entries):
       total_tag_duration = get_duration_for_entries(tagged_entries[tag])
       print "-" * (pad_spaces+21)
-      print "- %s - %s - %0d %%" % (pad(tag, pad_spaces-2),
-        get_human_readable_duration(total_tag_duration),
-        round(100*total_tag_duration/total_duration))
+      print "- %s - %s - %0d %%" % (pad(tag, pad_spaces-2), 
+        get_human_readable_duration(total_tag_duration), 
+        round(100*total_tag_duration/total_duration)) 
       print "-" * (pad_spaces+21)
       for entry in tagged_entries[tag]:
         print str(entry)
@@ -542,15 +542,14 @@ def summarize(goals, user_data, for_tags = False):
   else:
     print_effort(desc_entries, start_time, end_time, pad_spaces=25)
 
-
+  
 def fancy_tri_column_print(a, b, c, col_width, spacing):
   longest_column = max(len(a), len(b), len(c))
   rowStr = ""
-  for title in ("Alphabetical", "Frequent", "Recent"):
+  for title in ("Optimal", "Frequent", "Recent"):
     rowStr += title + " "*(col_width-len(title))
     rowStr += " " * spacing
   print(rowStr.strip())
-  a = sorted(a, key=lambda goal: goal.description.lower())
   for i in range(0, longest_column):
     rowStr = ""
     for col in (a,b,c):
@@ -568,20 +567,20 @@ def fancy_tri_column_print(a, b, c, col_width, spacing):
         rowStr += " " * spacing
     print rowStr
   print ""
-
+      
 def get_most_recent_goals(goals, limit):
   goals.sort(key=lambda goal: goal.last_updated_at)
   selected_goals = goals[-1*limit:]
   selected_goals.reverse()
   return selected_goals
-
+  
 def get_most_frequent_goals(goals, limit, time_limit):
   min_time = NOW - time_limit
   relevant_goals = [goal for goal in goals if goal.last_updated_at > min_time]
   relevant_goals.sort(key=lambda goal: goal.get_effort_in_interval(min_time, NOW))
   relevant_goals.reverse()
   return relevant_goals[:limit]
-
+  
 def get_optimal_goals(goals, limit):
   goals.sort(key=lambda goal: goal.value_rate)
   goals.reverse()
@@ -591,7 +590,7 @@ def pad(data, maxLen):
       if len(data) < maxLen:
         return data + " " * (maxLen - len(data))
       return data[:maxLen]
-
+  
 class Entry():
   def __init__(self, progress_data, goal):
     self.start_time = progress_data[0]
@@ -630,10 +629,10 @@ class MultiEntry():
     self.start_time = entry.start_time
     self.end_time = entry.end_time
     self.duration = entry.duration
-
+    
   def is_same_time(self, entry):
     return self.start_time == entry.start_time and self.end_time == entry.end_time
-
+    
   def add(self, entry):
     self.entries.append(entry)
 
@@ -651,13 +650,13 @@ class MultiEntry():
   @property
   def notes(self):
     return '|'.join([entry.notes for entry in self.entries])
-
+    
   def get_goal_amount_pairs(self):
     goal_amount_pairs = []
     for entry in self.entries:
       goal_amount_pairs.append([entry.goal, entry.focus])
     return goal_amount_pairs
-
+    
   def __str__(self):
     def pad(data, maxLen):
       if len(data) < maxLen:
@@ -673,7 +672,7 @@ class MultiEntry():
     tag_string = pad("|".join(tag_set), 10)
     descriptions = " and ".join([entry.goal.description[:40] for entry in self.entries])
     return "[%s] %s to %s (for %.2d:%.2d) %s" % (tag_string, formatted_starttime, formatted_endtime, hours, minutes, descriptions)  + ': ' + entry.notes
-
+    
 def get_multi_entries_since(goals, start_time):
   recent_goals = []
   for goal in goals:
@@ -715,10 +714,10 @@ def get_entries_in_period(goals, start_time, end_time):
         entries.append(entry_object)
   entries.sort(key=lambda x: x.start_time)
   return entries
-
+  
 def display_record(entries):
-  print "\n".join([str(entry) for entry in entries]) + "\n"
-
+  print "\n".join([str(entry) for entry in entries]) + "\n" 
+    
 def main():
   NUM_TO_SHOW = 40
   #this should only happen on windows
@@ -735,7 +734,6 @@ def main():
   all_recent_goals = get_most_recent_goals(goals, NUM_TO_SHOW)
   recent_entries = get_multi_entries_since(all_recent_goals, NOW - one_week_in_seconds)
   display_record(recent_entries[-1*NUM_TO_SHOW:])
-  prev_entry = None
   if len(recent_entries) > 0:
     prev_entry = recent_entries[-1]
   #use tags to more sensibly display the most recent tasks, most frequent tasks, and highest value tasks
@@ -780,7 +778,7 @@ def main():
   display_record(recent_entries[-10:])
   #finally, prompt for any input so that the window doesnt close instantly
   raw_input()
-
+    
 if __name__ == "__main__":
   main()
-
+  
